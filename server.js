0095ba554dfb5a375d1ea0a72eb9e1a4e2ff3bd7
Eric MULTFER <eric.faria2003@gmail.com>
2025-05-23 10:16:08 -0300

    Ajustado porta do servidor

--- server.js
+++ server.js
@@ -32,4 +32,7 @@ app.get('/api/products', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor rodando em http://localhost:${PORT}`));
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Servidor rodando em http://0.0.0.0:3000');
+});
+
